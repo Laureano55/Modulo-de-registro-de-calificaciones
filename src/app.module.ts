2bebeb0ba3f17e4ac496b162eaec7b3c5b45c314
Jesus Cabrera <jcabreradelatorre@gmail.com>
2025-08-20 05:53:49 -0500

    createGrade service implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GradesModule } from './modules/grades/grades.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GradesModule]
 })
 export class AppModule {}
